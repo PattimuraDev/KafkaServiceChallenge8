0bce4f788f91c46dea7f8c3bb5f31638ff3d949b
PattimuraDev <dwisatriapatra2000@gmail.com>
2022-12-28 17:11:28 +0700

    Update swagger server list

--- src/main/java/org/example/configuration/SwaggerConfig.java
+++ src/main/java/org/example/configuration/SwaggerConfig.java
@@ -3,9 +3,13 @@ package org.example.configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * kelas untuk representasi konfigurasi swagger ui
@@ -24,6 +28,11 @@ public class SwaggerConfig {
     public OpenAPI demoApi(@Value("REST API kafka microservice for challenge chapter 7 backend java Binar Academy") String appDescription,
                            @Value("v1.0.0") String appVersion
     ){
+        Server server = new Server();
+        server.setUrl("https://kafka-microservice-challenge8.up.railway.app/");
+        List<Server> listOfServer = new ArrayList<>();
+        listOfServer.add(server);
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Kafka Microservice Endpoints")
@@ -32,7 +41,8 @@ public class SwaggerConfig {
                         .termsOfService("http://swagger.io/terms")
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("http://springdoc.org")));
+                                .url("http://springdoc.org"))
+                ).servers(listOfServer);
     }
 
 }
